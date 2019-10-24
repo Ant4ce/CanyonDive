ad4d4158484582a4b9b7cc8f9c9135ce5732223a
Ant4ce <43274171+Ant4ce@users.noreply.github.com>
2019-10-24 13:52:16 +0200

    feature(score script): Score  counter for canvas
    Added text component to new UI element in Base Scene and a new script
    for it that calculates the players score based on height.
    
    development - #3 W.W.

---
+++ Assets/Scripts/Canvas scripts/ScoreUpkeep.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpkeep : MonoBehaviour
+{
+    private float startingheight;
+    Text score;
+    public GameObject BirdToFollow;
+    private static int startheight = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text> ();
+        startingheight = BirdToFollow.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Score: " + Mathf.Floor(BirdToFollow.transform.position.y - startingheight);
+    }
+}
