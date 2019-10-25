da54bf4937045735f351f8cf4d93e46e46b9eda2
Ant4ce <43274171+Ant4ce@users.noreply.github.com>
2019-10-25 12:39:59 +0200

    fix(Assets): file removal
    Remove many unused UI files that were taking up space and causing
    conflict.
    
    development #4 W.W.

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    // creates a slider for the speedMultiplier variable that allows it to be changed in Unity UI
+    // speedMultiplier is variable to be able to change speed of camera 
     [SerializeField]
     [Range(0f, 20f)]
     private float speedMultiplier = 1f;
-
+    //variable to hold and update the y position of the player object
     private float fheight;
+    //
     private float rapidite = 1f;
 
     [SerializeField]
--- Assets/Scripts/Canvas scripts/ScoreUpkeep.cs
+++ Assets/Scripts/Canvas scripts/ScoreUpkeep.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreUpkeep : MonoBehaviour
-{
+{   
+    // variable to save starting height, used for score calculation
     private float startingheight;
+    // variable to assign text to
     Text score;
+    // making the GameObject variable so it can be assigned the bird prefab in unity UI 
     public GameObject BirdToFollow;
-    private static int startheight = 1;
-
 
     // Start is called before the first frame update
+    
     void Start()
     {
+        // assigning the unity Text component to text variable "score"  
         score = GetComponent<Text> ();
+        
+        // saving the starting height of the bird before the update method starts looping
         startingheight = BirdToFollow.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // changing the content of score variable to "Score: " and calculating score based on y position of bird player - the starting height of the player
+        // Mathf.floor is used to round down to the largest whole number so that we dont see all the decimal numbers
         score.text = "Score: " + Mathf.Floor(BirdToFollow.transform.position.y - startingheight);
     }
 }
