8154d0c38c0a6026006cf5127ef44bfd3fa7b913
fbachus <felix.bachstein@gmail.com>
2019-10-28 11:13:10 +0100

    feature(Platforms): distance is more random
    Platforms are now generated in unregular distance to each other
    
    known Problems:
    After 100 height, distance between Platforms becomes to big -
    logarithmic distance seems advisable
    Platforms are not destroyed after getting below player

--- Assets/Scripts/Platforms/PlatformGen.cs
+++ Assets/Scripts/Platforms/PlatformGen.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
 using Random = UnityEngine.Random;
 using Vector3 = UnityEngine.Vector3;
 
@@ -10,44 +11,49 @@ public class PlatformGen : MonoBehaviour
 {
     public BoxCollider2D platform1;
     public Rigidbody2D player;
+    
     private Vector3 lastPlatformPosition;
-    private float[] horizontalRange = {-2.5f, 2.5f};
+    private Vector3 currentposition;
+    private Quaternion currentrotation;
+    private float[] horizontalRange = {-2.5f, 2.5f}; //TODO: adept flexibly to screen size
     private float camHorizontalPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         Vector3 cameraPosition = GetComponent<Transform>().position;
+        currentposition = GetComponent<Transform>().position;
+        currentrotation = GetComponent<Transform>().rotation;
         camHorizontalPosition = cameraPosition.x;
         lastPlatformPosition = cameraPosition;
         lastPlatformPosition.y = lastPlatformPosition.y - 5f;
         lastPlatformPosition.z = player.transform.position.z;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
-
+    
+    //get current position and generate Platforms in limited range above
     private void FixedUpdate()
     {
-        var current = GetComponent<Transform>();
-        if (lastPlatformPosition.y <= current.position.y + 100f)
+        var height = currentposition.y;
+        if (lastPlatformPosition.y <= height + 100f)
         {
             BoxCollider2D Platforms;
-            Vector3 newPlatformPosition = NewPlatform(lastPlatformPosition, horizontalRange);
-            Platforms = Instantiate(platform1, newPlatformPosition , current.rotation) as BoxCollider2D;
+            Vector3 newPlatformPosition = NewPlatform(lastPlatformPosition, horizontalRange, height);
+            Platforms = Instantiate(platform1, newPlatformPosition , currentrotation) as BoxCollider2D;
         }
     }
     
-    //This is the method defining the Platforms distance
-    private Vector3 NewPlatform(Vector3 oldPlatform, float[] horizontalRange)
+    //This is the method defining the Platforms position and distance
+    private Vector3 NewPlatform(Vector3 oldPlatform, float[] horizontalRange, float height)
     {
         Vector3 newPlatform;
         newPlatform.x = camHorizontalPosition + Random.Range(horizontalRange[0],horizontalRange[1]);
-        newPlatform.y = oldPlatform.y + 5f;
+        var randomHeightMod = height * Random.Range(0.02f, 0.026f);
+        newPlatform.y = 3f + oldPlatform.y + 1.01f * height + Random.Range(-randomHeightMod, randomHeightMod);
         newPlatform.z = oldPlatform.z;
         lastPlatformPosition = newPlatform;
         return newPlatform;
