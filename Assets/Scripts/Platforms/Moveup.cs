538c56be3c6e8810c500747788ca0eb65cb95b01
fbachus <felix.bachstein@gmail.com>
2019-10-28 08:09:50 +0100

    feature(platforms): add first iteration
    add first version of platform generation
    platforms are currently 2D Rigidbodies
    could be put on Tilemap in the future
    
    known problems:
    on collision the player object starts rotating
    the platform objects are not destroyed after getting below player
    platforms have fixed distance to each other

---
+++ Assets/Scripts/Platforms/Moveup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var campos = GetComponent<Transform> ();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Time.deltaTime * 5f * UnityEngine.Vector3.up;
+        }
+    }
+}
---
+++ Assets/Scripts/Platforms/PlatformGen.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlatformGen : MonoBehaviour
+{
+    public BoxCollider2D platform1;
+    public Rigidbody2D player;
+    private Vector3 lastPlatformPosition;
+    private float[] horizontalRange = {-2.5f, 2.5f};
+    private float camHorizontalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 cameraPosition = GetComponent<Transform>().position;
+        camHorizontalPosition = cameraPosition.x;
+        lastPlatformPosition = cameraPosition;
+        lastPlatformPosition.y = lastPlatformPosition.y - 5f;
+        lastPlatformPosition.z = player.transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        var current = GetComponent<Transform>();
+        if (lastPlatformPosition.y <= current.position.y + 100f)
+        {
+            BoxCollider2D Platforms;
+            Vector3 newPlatformPosition = NewPlatform(lastPlatformPosition, horizontalRange);
+            Platforms = Instantiate(platform1, newPlatformPosition , current.rotation) as BoxCollider2D;
+        }
+    }
+    
+    //This is the method defining the Platforms distance
+    private Vector3 NewPlatform(Vector3 oldPlatform, float[] horizontalRange)
+    {
+        Vector3 newPlatform;
+        newPlatform.x = camHorizontalPosition + Random.Range(horizontalRange[0],horizontalRange[1]);
+        newPlatform.y = oldPlatform.y + 5f;
+        newPlatform.z = oldPlatform.z;
+        lastPlatformPosition = newPlatform;
+        return newPlatform;
+    }
+}
