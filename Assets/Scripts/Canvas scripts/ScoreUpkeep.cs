c70b67cfb9941bb158e425ab64ac9c7e5df4ddc2
Ant4ce <43274171+Ant4ce@users.noreply.github.com>
2019-10-28 15:29:04 +0100

    Fixes(CameraScripts & scene): Ensuring the camera doesnt bug and better scene
    Fixes applied to the CameraFollow script to remove the visual bug on
    movement. Updated the scene for better testing, adding some objects for
    reference higher up. Minor changes to CameraMove.
    
    feature_Camera_Fix #1 W.W

--- Assets/Scripts/Canvas scripts/ScoreUpkeep.cs
+++ Assets/Scripts/Canvas scripts/ScoreUpkeep.cs
@@ -7,18 +7,15 @@ public class ScoreUpkeep : MonoBehaviour
 {   
     // variable to save starting height, used for score calculation
     private float startingheight;
-    // variable to assign text to
     Text score;
     // making the GameObject variable so it can be assigned the bird prefab in unity UI 
     public GameObject BirdToFollow;
 
-    // Start is called before the first frame update
+    
     
     void Start()
-    {
-        // assigning the unity Text component to text variable "score"  
+    {  
         score = GetComponent<Text> ();
-        
         // saving the starting height of the bird before the update method starts looping
         startingheight = BirdToFollow.transform.position.y;
     }
@@ -26,7 +23,6 @@ public class ScoreUpkeep : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // changing the content of score variable to "Score: " and calculating score based on y position of bird player - the starting height of the player
         // Mathf.floor is used to round down to the largest whole number so that we dont see all the decimal numbers
         score.text = "Score: " + Mathf.Floor(BirdToFollow.transform.position.y - startingheight);
     }
