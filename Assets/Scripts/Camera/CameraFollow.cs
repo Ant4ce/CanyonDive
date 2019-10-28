a16725d6631afe131d4f742a06134f60e79347a8
Ant4ce <43274171+Ant4ce@users.noreply.github.com>
2019-10-28 15:29:04 +0100

    Fixes(CameraScripts & scene): Ensuring the camera doesnt bug and better scene
    Fixes applied to the CameraFollow script to remove the visual bug on
    movement. Updated the scene for better testing, adding some objects for
    reference higher up. Minor changes to CameraMove.
    
    feature_Camera_Fix #1 W.W

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -8,7 +8,7 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     public float smoothingspeed = 0.2f;
   
-    public float min_Y_offset = -2.6f;
+    public float min_Y_offset = 2.6f;
     private bool follow_player;
 
     // these variables are used for the Smoothstlk function, the ones below are only used for the Smoothstalk script. wherease the ones above are also used in update
@@ -20,28 +20,23 @@ public class CameraFollow : MonoBehaviour
     private float speed = 2.0f;
 
 
-    private void Update()
+    private void FixedUpdate()
     {
         //Follow();
 
         //use the one below or above not both at the same time
 
-        if (target.position.y < (transform.position.y - min_Y_offset))
-        {
-
-        }
-        if (target.position.y > transform.position.y)
-        {
-            Smoothstalk();
-        }
+        Smoothstalk();
     }
-
+    
+    //method to follow player by spaing to the position of the player
     void Follow()
     {
-        if (target.position.y < (transform.position.y - min_Y_offset ))
+        //makes the camera stop following player if player is bellow certain threshold 
+        if (target.position.y < (transform.position.y + min_Y_offset ))
         {
             follow_player = false;
-        }
+        }// camera follows player if above threshold
         if (target.position.y > transform.position.y)
         {
             follow_player = true;
@@ -56,13 +51,19 @@ public class CameraFollow : MonoBehaviour
         
     }
 
+    // method that follows the player but doesnt snap to player instantly, rather it catches up
     void Smoothstalk()
     {
+        // multiply by deltatime to make sure it is independant of frame rate
         float interpolation = speed * Time.deltaTime;
 
         Vector3 positiontobe = this.transform.position;
 
-        positiontobe.y = Mathf.Lerp(this.transform.position.y, thingToFollow.transform.position.y, interpolation);
+        //use lerp to smoothly transition the camera to new position
+        //positiontobe.y = Mathf.Lerp(this.transform.position.y, thingToFollow.transform.position.y, interpolation);
+
+        //Mathf.Max is used here to ensure that the lerp only can follow the player up not down. 
+        positiontobe.y = Mathf.Lerp(this.transform.position.y, Mathf.Max(thingToFollow.transform.position.y, this.transform.position.y), interpolation);
 
         this.transform.position = positiontobe;
     }
