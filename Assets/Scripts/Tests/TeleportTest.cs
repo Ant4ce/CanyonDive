1f50de913b8f3c8d3df352775010ee2de1356646
Alexander Pieczka <alexanderpieczka@ewe.net>
2019-11-17 14:07:48 +0100

    Testing(TDD): Ensures unit tests are being run, so that refactored code doesn't break functionallity Added Resources folder with relevant Prefabs inside (this can be refined later to not use on system files) Added a Folder to Scripts : Tests, which contains all testing scripts Added Test for Teleportation. Currently checking for relevant Prefabs.

---
+++ Assets/Scripts/Tests/TeleportTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TeleportTest
+    {
+        //REsources folder is not ideal. Works for current tests, but should be replaced so the files aren''t loaded from the system. (Some kind of service system maybe)
+        [UnityTest]
+        public IEnumerator TeleportInstantiatesGameObjectFromPrefab()
+        {
+            var weaponPrefab = Resources.Load("Tests/Weapon");
+            var weaponSpawnPrefab = Resources.Load("Tests/weaponStart");
+            
+            var thrownProjectile = GameObject.FindWithTag("weapon");
+            var throwStart = GameObject.FindWithTag("WeaponSpawn");
+            var prefabOfThrownWeapon = PrefabUtility.GetPrefabParent(thrownProjectile);
+            var prefabOfWeaponSpawner = PrefabUtility.GetPrefabParent(throwStart);
+            
+            Assert.AreEqual(weaponPrefab, prefabOfThrownWeapon);
+            Assert.AreEqual(weaponSpawnPrefab, prefabOfWeaponSpawner);
+            
+            yield return null;
+        }
+
+        [TearDown]
+        public void AfterAllTests()
+        {
+            foreach (var gameObject in GameObject.FindGameObjectsWithTag("weapon"))
+                Object.Destroy(gameObject);
+        }
+    }
+}
